9faf78e23bac783e572dc6254f62e94b393db3dd
Arvindh <64023913+Arvindh12@users.noreply.github.com>
2020-06-24 01:17:00 +0530

    Add files via upload
    Added option to change entries count

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var entriesPerPage = 10;
 var currentPage = 0;
 var totalpages ;
 
-fetch('https://raw.githubusercontent.com/angular-ui/ui-grid/master/misc/site/data/100.json')
+fetch('https://gist.githubusercontent.com/rvsp/add40254aa126f045837fa5b51f47f1f/raw/4d724bfabf4cce7379a386e23bef6576ab99a2f9/pagination.json')
   .then(response => response.json())
   .then(data => {
       storedData=data;
       totalpages=Math.ceil(storedData.length / entriesPerPage);
       pagination(data);
+      renderNav(0)
     });
 
 
@@ -17,12 +18,13 @@ function pagination (data){
     var table = document.getElementById("tb")
     table.innerHTML = "";
     for(let i=currentPage*entriesPerPage;i<currentPage*entriesPerPage+entriesPerPage;i++){
+        console.log(i)
         if (i>=storedData.length) return
         table.innerHTML += `<tr>
         <th scope="row">${i+1}</th>
-        <td>${data[i].name}</td>
-        <td>${data[i].gender}</td>
-        <td>${data[i].company}</td>
+        <td>${data[i].name.split(" ")[0]}</td>
+        <td>${data[i].name.split(" ")[1]}</td>
+        <td>${data[i].email}</td>
       </tr>`
     }
 }
@@ -47,26 +49,31 @@ function renderNav(val){
     nav.innerHTML = ""
     totalpages =  Math.ceil(storedData.length / entriesPerPage)
 
-    nav.innerHTML += ` <li class="page-item ${val == 0 ? "disabled": " "}" id="" onclick="updateNav('p')"><a class="page-link" href="#">Previous</a></li>`
+    nav.innerHTML += ` <li class="page-item ${val == 0 ? "disabled": " "}"  onclick="updateNav('p')"><a class="page-link" href="#">Previous</a></li>`
     if (val == 0 ){
-        nav.innerHTML += ` <li class="page-item active" id="" onclick="updateNav(0)"><a class="page-link" href="#">1</a></li>
-          <li class="page-item" id="" onclick="updateNav(1)"><a class="page-link" href="#">2</a></li>
-          <li class="page-item" id="" onclick="updateNav(2)"><a class="page-link" href="#">3</a></li> `
+        nav.innerHTML += ` <li class="page-item active"  onclick="updateNav(0)"><a class="page-link" href="#">1</a></li>
+          <li class="page-item" onclick="updateNav(1)"><a class="page-link" href="#">2</a></li>
+          <li class="page-item" onclick="updateNav(2)"><a class="page-link" href="#">3</a></li> `
     }
     else if( val == totalpages-1){
-        nav.innerHTML += ` <li class="page-item" id="" onclick="updateNav(${val-2})"><a class="page-link" href="#">${val-1}</a></li>
-        <li class="page-item" id="" onclick="updateNav(${val-1})"><a class="page-link" href="#">${val}</a></li>
-        <li class="page-item active" id="" onclick="updateNav(${val})"><a class="page-link" href="#">${val+1}</a></li> `
+        nav.innerHTML += ` <li class="page-item" onclick="updateNav(${val-2})"><a class="page-link" href="#">${val-1}</a></li>
+        <li class="page-item" onclick="updateNav(${val-1})"><a class="page-link" href="#">${val}</a></li>
+        <li class="page-item active" onclick="updateNav(${val})"><a class="page-link" href="#">${val+1}</a></li> `
     }
     else{
     for(let i=-1 ; i<2 ;i++){
         console.log((i == 0) ? "active" : "")
-        nav.innerHTML += `<li class="page-item ${ (i == 0) ? "active" : ""}" id="" onclick="updateNav(${i+val})"><a class="page-link" href="#">${val+i+1}</a></li>`
+        nav.innerHTML += `<li class="page-item ${ (i == 0) ? "active" : ""}" onclick="updateNav(${i+val})"><a class="page-link" href="#">${val+i+1}</a></li>`
     }
     }
-    nav.innerHTML += `<li class="page-item  ${val == totalpages-1 ? "disabled": " "}" id="" onclick="updateNav('n')"><a class="page-link" href="#">Next</a></li>`
+    nav.innerHTML += `<li class="page-item  ${val == totalpages-1 ? "disabled": " "}" onclick="updateNav('n')"><a class="page-link" href="#">Next</a></li>`
 }
 
 function changeEntries(e){
-console.log(e.value)
+entriesPerPage = Number(e.value);
+console.log(entriesPerPage,e.value)
+currentPage = 0;
+renderNav(currentPage)
+    pagination(storedData)
+
 }
